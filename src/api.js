206e1b5efcdda276479a204476f7ffcc6f40a678
Sagar Debnath <sagardebnath1001@gmail.com>
2024-07-23 17:13:10 +0530

    core functionality built.

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '4ea33f4cb5msh259213d94eb88a3p1220d1jsn0947f1997a9e',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL='https://api.openweathermap.org/data/2.5' 
+export const WEATHER_API_KEY='e548cd1a4d16e5aa4f1a557daf2c48af'
